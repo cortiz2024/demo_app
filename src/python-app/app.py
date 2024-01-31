075a61324fc7616958354a93504a366c5936ecb7
Cesar Ortiz <anortiz@andreani.com>
2024-01-31 09:33:39 -0500

    feat: v6.0.0

--- src/python-app/app.py
+++ src/python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_get():
-    return '<h1>Hello, World v3!</h1>'
+    return '<h1>Hello, World v4!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
